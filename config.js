6a86b79523c70a82c19b6237b46ecd0edbd6093c
Davis Kim <davis@blackbird.am>
2015-10-06 16:44:05 -0700

    update hbs dep ver and remove handlebars direct dep

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 System.config({
-  "baseURL": "/",
-  "defaultJSExtensions": true,
-  "transpiler": "babel",
-  "babelOptions": {
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: "babel",
+  babelOptions: {
     "optional": [
       "runtime"
     ]
   },
-  "paths": {
+  paths: {
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
-  }
-});
+  },
 
-System.config({
-  "map": {
+  map: {
     "babel": "npm:babel-core@5.7.1",
     "babel-core": "npm:babel-core@5.7.1",
     "babel-runtime": "npm:babel-runtime@5.6.17",
@@ -24,13 +22,12 @@ System.config({
     "backbone.wreqr": "github:marionettejs/backbone.wreqr@1.3.3",
     "core-js": "npm:core-js@0.9.18",
     "css": "github:systemjs/plugin-css@0.1.13",
-    "handlebars": "github:components/handlebars.js@4.0.2",
-    "hbs": "github:davis/plugin-hbs@1.0.0",
+    "hbs": "github:davis/plugin-hbs@1.2.0",
     "jquery": "github:components/jquery@2.1.4",
     "marionette": "github:marionettejs/backbone.marionette@2.4.2",
     "underscore": "npm:underscore@1.8.3",
-    "github:davis/plugin-hbs@1.0.0": {
-      "handlebars": "github:components/handlebars.js@3.0.3"
+    "github:davis/plugin-hbs@1.2.0": {
+      "handlebars": "github:components/handlebars.js@4.0.3"
     },
     "github:jspm/nodelibs-path@0.1.0": {
       "path-browserify": "npm:path-browserify@0.0.0"
@@ -62,4 +59,3 @@ System.config({
     }
   }
 });
-
