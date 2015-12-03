c77b09b8bf0bc40f5df8a2a46f1f43aecac5dba4
Davis Kim <davis@blackbird.am>
2015-12-02 16:14:16 -0800

    add helper example

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 'use strict';
 
 import Backbone from 'backbone';
+import Handlebars from 'handlebars/handlebars.runtime'; // use runtime environment to ensure helpers work
 
 import Application from './app/application';
+
+Handlebars.registerHelper('wrapWithMoo', (options) => {
+  return new Handlebars.SafeString(`moo! ${options.fn(this)} moo!`);
+});
 
 const app = new Application();
 
